0902448c7ad0d189868defe8718b4d873d1baf4d
oni113 <miskw99@gmail.com>
2024-12-26 09:35:13 +0900

    indent space 4

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    proxy: {
-      '/server': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/server/, '')
-      }
+    plugins: [react()],
+    server: {
+        port: 3001,
+        proxy: {
+            '/server': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/server/, '')
+            }
+        }
     }
-  }
 })
